8d3b9bf1a58db3071fb4871fd5c7a572968cd6b5
neko <447140443@qq.com>
2021-07-09 22:02:33 +0800

    test general_utils

--- classify_leaves/general_utils.py
+++ classify_leaves/general_utils.py
@@ -6,3 +6,24 @@ import os
 import numpy as np
 
 import torch
+
+
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    # 使用了多GPU需要加上module
+    checkpoint = {
+        "state_dict": model.module.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location='cuda:0')
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
